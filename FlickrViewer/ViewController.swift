045bc34c1c9303091b9d71e418749f7a97395a40
Hiroshi Tazawa <convexstyle@gmail.com>
2016-02-23 22:12:26 +1100

    first commit

---
+++ FlickrViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrViewer
+//
+//  Created by Hiroshi Tazawa on 23/02/2016.
+//  Copyright © 2016 convexstyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
